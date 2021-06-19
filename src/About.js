e40975c3cf6b2e91c70bf76697a8440eccf9142a
Luis Bonilla <blessedluismi@gmail.com>
2021-06-18 21:55:06 -0400

    updates

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About(props){
+    return (
+        <div><h1>new comp</h1></div>
+    )
+}
+
+export default About;
---
+++ src/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Projects(props){
+    return (
+        <div><h1>new comp</h1></div>
+    )
+}
+
+export default Projects;
